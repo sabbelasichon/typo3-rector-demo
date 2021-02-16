ef7bea932ab2335cfbe3d4ee1250a3d7fa4d1404
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2021-02-16 20:01:45 +0100

    [TASK] Add some examples to rewrite with custom rules

--- packages/legacy_extension/Classes/Service/MyService.php
+++ packages/legacy_extension/Classes/Service/MyService.php
@@ -5,7 +5,21 @@ declare(strict_types=1);
 namespace Ssch\LegacyExtension\Service;
 
 
+use TYPO3\CMS\Core\Utility\StringUtility;
+
 final class MyService
 {
+    /**
+     * @var string
+     */
+    private const FOO = 'foo';
 
+    public function removeFooFromTheBeginningOfTheStringCaseInsensitive(string $string): string
+    {
+        if(StringUtility::beginsWith(strtolower($string), self::FOO)) {
+            return substr($string, strlen(self::FOO));
+        }
+
+        return $string;
+    }
 }
